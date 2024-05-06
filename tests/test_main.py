12247da4079ef30cda26d7ffd91c1c5ce960a36e
Klaas <k.j.sicking@student.tudelft.nl>
2024-05-06 22:00:33 +0200

    test setup

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,9 @@
 """Test module for the package."""
 
+
 class TestClass:
     """Test class for the package."""
 
     def test_method(self) -> None:
         """Test the method."""
         assert True
-
