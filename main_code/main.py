f423e3f3167bc7a7a9d67026ceff4364341e910f
changjunlicj <changjunli0118@gmail.com>
2024-05-15 10:53:41 +0200

    add the visualization of model

--- main_code/main.py
+++ main_code/main.py
@@ -1,10 +1,18 @@
 """Module for main code."""
 from icecream import ic
-
+from main_code.network import AcyclicNetworkGenerator
 from main_code.optimisation_model import OptimisationModel
+from main_code.set_params import NetworkParameters
+
+create_figure = True
 
 if __name__ == "__main__":
     optimisation_model = OptimisationModel()
     optimisation_model.solve()
     ic(optimisation_model.network.create_cost_table)
     optimisation_model.results()
+
+    if create_figure:
+        network_params = NetworkParameters()
+        network = AcyclicNetworkGenerator(params=network_params)
+        network.visualize_network()
